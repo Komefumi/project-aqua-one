36436feb2f1d3c74f1efddc19667a3536b64d426
Hafiz <komefumi@protonmail.ch>
2023-08-26 08:02:07 +0530

    Initializes Plugin

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+  Plugin Name: Project Aqua One
+  Description: A Plugin
+  Version: 0.1
+  Author: Vecktor [komefumi]
+  Author URI: komefumi.github.io
+*/
+
+if (!defined('ABSPATH')) exit; // exit if accessed directly
